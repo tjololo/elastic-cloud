6398a77db903b9a13f1e310a7320d22d4d59858e
Vemund Gaukstad <tjololo@users.noreply.github.com>
2021-12-03 10:53:18 +0100

    deploy example ec (#2)
    * deploy example ec

---
+++ elastic.tf
@@ -0,0 +1,25 @@
+data "ec_stack" "latest" {
+  version_regex = "latest"
+  region        = "azure-westeurope"
+}
+
+provider "ec" {
+}
+
+resource "ec_deployment" "example_azure" {
+  # Optional name.
+  name = "example-setup"
+
+  # Mandatory fields
+  region                 = data.ec_stack.latest.region
+  version                = data.ec_stack.latest.version
+  deployment_template_id = "azure-io-optimized-v3"
+
+  elasticsearch {}
+
+  kibana {}
+
+  apm {}
+
+  enterprise_search {}
+}
