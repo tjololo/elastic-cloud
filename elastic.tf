f36ef8a4047838553ff7c140be64236ceda15eed
Vemund Gaukstad <tjololo@users.noreply.github.com>
2022-01-13 17:25:46 +0100

    setup new demo

--- elastic.tf
+++ elastic.tf
@@ -1,74 +1,74 @@
-# data "ec_stack" "latest" {
-#   version_regex = "latest"
-#   region        = "azure-westeurope"
-# }
+data "ec_stack" "latest" {
+  version_regex = "latest"
+  region        = "azure-westeurope"
+}
 
-# resource "ec_deployment" "example_azure" {
-#   # Optional name.
-#   name = "example-setup"
+resource "ec_deployment" "example_azure" {
+  # Optional name.
+  name = "example-setup"
 
-#   # Mandatory fields
-#   region                 = data.ec_stack.latest.region
-#   version                = data.ec_stack.latest.version
-#   deployment_template_id = "azure-io-optimized-v3"
+  # Mandatory fields
+  region                 = data.ec_stack.latest.region
+  version                = data.ec_stack.latest.version
+  deployment_template_id = "azure-io-optimized-v3"
 
-#   elasticsearch {
-#     autoscale = "false"
+  elasticsearch {
+    autoscale = "false"
 
-#     topology {
-#       id         = "hot_content"
-#       size       = "1g"
-#       zone_count = 1
-#     }
+    topology {
+      id         = "hot_content"
+      size       = "4g"
+      zone_count = 2
+    }
 
-#     topology {
-#       id   = "warm"
-#       size = "0g"
-#     }
+    topology {
+      id   = "warm"
+      size = "0g"
+    }
 
-#     topology {
-#       id   = "cold"
-#       size = "0g"
-#     }
+    topology {
+      id   = "cold"
+      size = "0g"
+    }
 
-#     topology {
-#       id   = "coordinating"
-#       size = "0g"
-#     }
+    topology {
+      id   = "coordinating"
+      size = "0g"
+    }
 
-#     topology {
-#       id   = "frozen"
-#       size = "0g"
-#     }
+    topology {
+      id   = "frozen"
+      size = "0g"
+    }
 
-#     topology {
-#       id   = "ml"
-#       size = "0g"
-#     }
+    topology {
+      id   = "ml"
+      size = "0g"
+    }
 
-#     topology {
-#       id   = "master"
-#       size = "0g"
-#     }
-#   }
+    topology {
+      id   = "master"
+      size = "0g"
+    }
+  }
 
-#   kibana {
-#     topology {
-#       size       = "1g"
-#       zone_count = 1
-#     }
-#   }
+  kibana {
+    topology {
+      size       = "1g"
+      zone_count = 1
+    }
+  }
 
-#   apm {
-#     topology {
-#       size = "0.5g"
-#     }
-#   }
-#   enterprise_search {
-#     topology {
-#       size       = "2g"
-#       zone_count = 1
-#     }
+  apm {
+    topology {
+      size = "0.5g"
+    }
+  }
+  enterprise_search {
+    topology {
+      size       = "2g"
+      zone_count = 2
+    }
 
-#   }
-# }
+  }
+}
