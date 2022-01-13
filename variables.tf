0753f24f10dab6942823274f475259d4abc13f12
Vemund Gaukstad <tjololo@users.noreply.github.com>
2022-01-13 18:36:53 +0100

    Simpler configuration (#6)
    * test simpler config
    
    * scale down to zero

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "elastic_memory_size" {
+  type        = string
+  description = "hot_content memory size"
+}
+
+variable "elastic_zone_count" {
+  type        = number
+  description = "number of zones"
+}
+
+variable "enterprise_search_size" {
+  type        = string
+  description = "enterprise search memory size"
+}
+
+variable "enterprise_search_zone_count" {
+  type        = number
+  description = "number of zones"
+}
+
+variable "deploy_count" {
+  type        = number
+  description = "number of deployments"
+}
